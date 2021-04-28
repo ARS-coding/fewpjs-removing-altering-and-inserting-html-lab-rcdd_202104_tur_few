38b7f531dd2094e52826e6a64dd535726292fab1
Ali Riza <ars.lifestyle@hotmail.com>
2021-04-28 18:28:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ let main = document.body.querySelector("main#main");
 main.remove();
 
 document.createElement("h1");
-document.body.main.appendChild("h1");
+document.main.appendChild("h1");
 document.body.querySelector("h1").setAttribute("id", "victory");
 let newHeader = document.body.main.querySelector("h1#victory");
